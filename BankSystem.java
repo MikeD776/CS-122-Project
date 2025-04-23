50ead94226bdfdc5f1d856bfeaad8a3319f42e2d
Isaac Lasso Younes <isaaclassoyounes@gmail.com>
2025-04-23 17:21:57 -0400

    Here I have the starting code :D

---
+++ BankSystem.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class BankSystem {
+
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("Welcome to your bank system");
+            System.out.println();
+            System.out.println("For login type 1");
+            System.out.println("For creating an account type 2");
+            Scanner scnr = new Scanner(System.in);
+            String choice = scnr.nextLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# CS-122-Project
+Hey team!!
+
+Here we are going to creat the bank system project.
+
+If you have any questions please reach out to me.
+
+Do your best!
