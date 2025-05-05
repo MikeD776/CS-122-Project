9d58a41dbece3aa69957639082bb19664677a561
angelt32 <angelt3213@gmail.com>
2025-05-05 14:03:46 -0400

    looks pretty

--- BankSystem.java
+++ BankSystem.java
@@ -4,208 +4,209 @@ import java.util.Scanner;
 
 public class BankSystem {
 
-    public static class account{
+    public static class account {
         private String username;
         private String password;
         private double balance;
-        
-        public account(String username, String password){
+
+        public account(String username, String password) {
             this.username = username;
             this.password = password;
             this.balance = 0.0;
         }
-        
-        public String getUsername(){
+
+        public String getUsername() {
             return username;
         }
-        public String getPassword(){
+
+        public String getPassword() {
             return password;
         }
-        public double gatBalance(){
+
+        public double gatBalance() {
             return balance;
         }
 
-        public void deposit(double amount){
-            if(amount> 0){
-                balance +=amount;
+        public void deposit(double amount) {
+            if (amount > 0) {
+                balance += amount;
             }
         }
-        public boolean withdraw(double amount){
-            if(amount > 0 && amount<= balance){
-                balance-= amount;
+
+        public boolean withdraw(double amount) {
+            if (amount > 0 && amount <= balance) {
+                balance -= amount;
                 return true;
             }
             return false;
         }
-        public boolean transfer(double amount){
-           return false; 
-        }
 
         @Override
-        public String toString(){
-            return "Username: " + username + ", Balance: $ " + balance + password;
+        public String toString() {
+            return "Username: " + username + ", Balance: $" + balance;
         }
     }
-   
+
     static ArrayList<account> accounts = new ArrayList<>();
 
     public static void main(String[] args) {
-        while(true){
-            System.out.println("WELCOME TO YOUR BANK SYSTEM");
-            System.out.println();
-            System.out.println("For login type 1");
-            System.out.println("For creating an account type 2");
-            System.out.println("For closing the system type 3");
-            try{
-                Scanner scnr = new Scanner(System.in);
+        Scanner scnr = new Scanner(System.in);
+
+        while (true) {
+            displayMainMenu();
+            try {
                 System.out.print("Choice: ");
                 int choice = scnr.nextInt();
-            
-                if(choice == 1){
-                    //Login logic
-                    //Check if their is an existing user
-                    //In here we are going to have the bank system
-                    //We nee in the while loop logic for money withdraws, deposits, and transactions between accounts
-                    //For those we need to check if they have enough money and if there are other accounts(for the last one)
-                    //Add other things if you feel like is a good idea
-                    while(true){ 
-                        System.out.println("Login: ");
-                        System.out.print("Enter your username: ");
-                        String usernameLogin = scnr.next();
-                        System.out.print("Enter your password: ");
-                        String passwordLogin = scnr.next();
-                        
-                        boolean found = false;
-                        account loggedIn = null;
 
-                        for (account acc : accounts) {
-                            if(acc.getUsername().equals(usernameLogin) && acc.getPassword().equals(passwordLogin)) {
-                                found = true;
-                                loggedIn = acc; 
-                                break;
-                            }
-                        }
-                        if (!found) {
-                            System.out.println("Invalid username or password! Returning to main menu.");
-                            continue;
-                        }
-                        System.out.println("Login successful! Welcome, " + loggedIn.getUsername());
-
-                        while(true) {
-                            System.out.println("\nAccount Menu:");
-                            System.out.println("1. Check Balance");
-                            System.out.println("2. Deposit");
-                            System.out.println("3. Withdraw");
-                            System.out.println("4. Transfer");
-                            System.out.println("5. Logout");
-                            System.out.println("Choice: ");
-
-                            int action = scnr.nextInt();
-
-                            if(action == 1) {
-                                System.out.printf("Balance: $%.2f\n", loggedIn.gatBalance());
-                            } else if (action == 2) {
-                                System.out.print("Enter deposit amount: ");
-                                double amount = scnr.nextDouble();
-                                loggedIn.deposit(amount);
-                                System.out.println("Deposit successful!");
-                            } else if (action == 3) {
-                                System.out.print("Enter withdrawal amount: ");
-                                double amount = scnr.nextDouble();
-                                if (loggedIn.withdraw(amount)) {
-                                    System.out.println("Withdrawal successful!");
-                                } else {
-                                    System.out.println("Insufficient balacne.");
-                                }
-                            } else if (action == 4) {
-                                System.out.print("Enter recipient username: ");
-                                String recipientName = scnr.next();
-                                account recipient = null;
-                                for (account acc : accounts) {
-                                    if (acc.getUsername().equals(recipientName)) {
-                                        recipient = acc;
-                                        break;
-                                    }
-                                }
-                                if (recipient == null || recipient == loggedIn) {
-                                    System.out.println("Invalid recipient.");
-                                    continue;
-                                }
-
-                                System.out.print("Enter amount to transfer: ");
-                                double amount = scnr.nextDouble();
-                                if (loggedIn.withdraw(amount)) {
-                                    recipient.deposit(amount);
-                                    System.out.println("Transfer successful!");
-                                } else {
-                                    System.out.println("Insufficient balance for transfer.");
-                                }
-                            } else if (action == 5) {
-                                System.out.println("Logging out...");
-                                break;
-                            } else {
-                                System.out.println("Invalid option. Try again.");
-                            }
-                        }
-                        break;
-                    }
-                }
-
-
-                if(choice == 2){
-                    while(true){
-                        System.out.println("Enter new Username: ");
-                        String username = scnr.next();
-                        System.out.println("Enter new Password: ");
-                        System.out.println("Password must be at least 10 characters long, have a number, have a letter, and have a special character (.!@#$%^&*?)");
-                        String password = scnr.next();
-
-                        Boolean digit = false;
-                        Boolean letter = false;
-                        Boolean length = false;
-                        Boolean spChar = false;
-                        String specialChars = ".!@#$%^&*?";
-                        
-                        for(int i = 0; i < password.length(); i++){
-                            char character = password.charAt(i);
-                            if (Character.isDigit(character)){
-                                digit = true;
-                            }
-                            if(Character.isLetter(character)){
-                                letter = true;
-                            }
-                            if(specialChars.contains(String.valueOf(character))){
-                                spChar = true;
-                            }
-                            if(password.length() >= 10){
-                                length = true;
-                            }
-                        }
-                        if(digit && letter && spChar && length){
-                            System.out.println("Succesfully created password!!");
-                            account acc1 = new account(username, password);
-                            accounts.add(acc1);
-                            break;
-                        }
-                        else{
-                            System.out.println("Password is not complicated enough");
-                        }
-                    }
-                    continue;
-                }
-                if(choice ==3){
+                if (choice == 1) {
+                    handleLogin(scnr);
+                } else if (choice == 2) {
+                    handleAccountCreation(scnr);
+                } else if (choice == 3) {
                     System.out.println("\nThank you for using our bank system");
                     break;
-                }
-                else{
+                } else {
                     System.out.println("\nPLEASE ENTER ONE OF THE CHOICES!!! (1,2,3)\n");
                 }
-            }
-            catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("\nPLEASE ENTER ONE OF THE CHOICES!!!\n");
+                scnr.next(); 
             }
-            
         }
     }
+
     
+    public static void displayMainMenu() {
+        System.out.println("WELCOME TO YOUR BANK SYSTEM");
+        System.out.println();
+        System.out.println("For login type 1");
+        System.out.println("For creating an account type 2");
+        System.out.println("For closing the system type 3");
+    }
+
+    
+    public static void handleLogin(Scanner scnr) {
+        System.out.println("Login: ");
+        System.out.print("Enter your username: ");
+        String usernameLogin = scnr.next();
+        System.out.print("Enter your password: ");
+        String passwordLogin = scnr.next();
+
+        account loggedIn = findAccountByUsername(usernameLogin);
+
+        if (loggedIn == null || !loggedIn.getPassword().equals(passwordLogin)) {
+            System.out.println("Invalid username or password! Returning to main menu.");
+            return;
+        }
+
+        System.out.println("Login successful! Welcome, " + loggedIn.getUsername());
+        handleAccountMenu(scnr, loggedIn);
+    }
+
+    
+    public static void handleAccountCreation(Scanner scnr) {
+        while (true) {
+            System.out.println("Enter new Username: ");
+            String username = scnr.next();
+            System.out.println("Enter new Password: ");
+            System.out.println("Password must be at least 10 characters long, have a number, have a letter, and have a special character (.!@#$%^&*?)");
+            String password = scnr.next();
+
+            if (isPasswordComplex(password)) {
+                System.out.println("Successfully created password!!");
+                account acc1 = new account(username, password);
+                accounts.add(acc1);
+                break;
+            } else {
+                System.out.println("Password is not complicated enough");
+            }
+        }
+    }
+
+    
+    public static void handleAccountMenu(Scanner scnr, account loggedIn) {
+        while (true) {
+            System.out.println("\nAccount Menu:");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Transfer");
+            System.out.println("5. Logout");
+            System.out.print("Choice: ");
+
+            int action = scnr.nextInt();
+
+            if (action == 1) {
+                System.out.printf("Balance: $%.2f\n", loggedIn.gatBalance());
+            } else if (action == 2) {
+                System.out.print("Enter deposit amount: ");
+                double amount = scnr.nextDouble();
+                loggedIn.deposit(amount);
+                System.out.println("Deposit successful!");
+            } else if (action == 3) {
+                System.out.print("Enter withdrawal amount: ");
+                double amount = scnr.nextDouble();
+                if (loggedIn.withdraw(amount)) {
+                    System.out.println("Withdrawal successful!");
+                } else {
+                    System.out.println("Insufficient balance.");
+                }
+            } else if (action == 4) {
+                System.out.print("Enter recipient username: ");
+                String recipientName = scnr.next();
+                account recipient = findAccountByUsername(recipientName);
+
+                if (recipient == null || recipient == loggedIn) {
+                    System.out.println("Invalid recipient.");
+                    continue;
+                }
+
+                System.out.print("Enter amount to transfer: ");
+                double amount = scnr.nextDouble();
+                if (loggedIn.withdraw(amount)) {
+                    recipient.deposit(amount);
+                    System.out.println("Transfer successful!");
+                } else {
+                    System.out.println("Insufficient balance for transfer.");
+                }
+            } else if (action == 5) {
+                System.out.println("Logging out...");
+                break;
+            } else {
+                System.out.println("Invalid option. Try again.");
+            }
+        }
+    }
+
+    public static boolean isPasswordComplex(String password) {
+        boolean digit = false;
+        boolean letter = false;
+        boolean length = password.length() >= 10;
+        boolean spChar = false;
+        String specialChars = ".!@#$%^&*?";
+
+        for (int i = 0; i < password.length(); i++) {
+            char character = password.charAt(i);
+            if (Character.isDigit(character)) {
+                digit = true;
+            }
+            if (Character.isLetter(character)) {
+                letter = true;
+            }
+            if (specialChars.contains(String.valueOf(character))) {
+                spChar = true;
+            }
+        }
+        return digit && letter && spChar && length;
+    }
+
+    
+    public static account findAccountByUsername(String username) {
+        for (account acc : accounts) {
+            if (acc.getUsername().equals(username)) {
+                return acc;
+            }
+        }
+        return null;
+    }
 }
